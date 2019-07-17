f1739f4e1081ec283501e87609694e9bbc4b536d
Bonaro <46500752+Bonaro@users.noreply.github.com>
2019-07-17 13:00:34 -0300

    Add files via upload

---
+++ support.lua
@@ -0,0 +1,4 @@
+addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), function()
+    outputChatBox("Contribute to the project on GitHub")
+    outputChatBox("https://github.com/Bonaro/MTA-SA-PositionGUI-Edit")
+end)
