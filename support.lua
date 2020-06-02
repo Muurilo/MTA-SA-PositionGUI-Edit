6db18511176f3749f85d481444c0060a660120d4
Murilo Cunha <murilo.henriquecunha551@gmail.com>
2020-06-02 14:16:47 -0300

    Delete support.lua

--- support.lua
+++
@@ -1,4 +0,0 @@
-addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), function()
-    outputChatBox("Contribute to the project on GitHub")
-    outputChatBox("https://github.com/Bonaro/MTA-SA-PositionGUI-Edit")
-end)
